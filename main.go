999216d98bab47ddae8f75fc3855dafcdeedbfe2
Powwow84 <pao.saelee84@gmail.com>
2023-06-13 15:36:26 -0700

    added prometheus transformation

--- main.go
+++ main.go
@@ -6,17 +6,29 @@ import (
 	"log"
 	"os"
 	"time"
+
 	"github.com/gocolly/colly"
 )
 
 type WeatherData struct {
-	Day    string
-	High   string
-	Low    string
+	Day  string
+	High string
+	Low  string
+}
+
+func transformCSVToPrometheus(weatherData []WeatherData) (string, error) {
+	output := ""
+
+	for _, data := range weatherData {
+		output += fmt.Sprintf("high_temp{day=\"%s\"} %s\n", data.Day, data.High)
+		output += fmt.Sprintf("low_temp{day=\"%s\"} %s\n", data.Day, data.Low)
+	}
+
+	return output, nil
 }
 
 func main() {
-	data := make([]WeatherData, 0)
+	weatherData := make([]WeatherData, 0)
 	c := colly.NewCollector(colly.AllowedDomains("weather.com", "www.weather.com"))
 
 	c.OnRequest(func(r *colly.Request) {
@@ -27,7 +39,7 @@ func main() {
 		day := e.ChildText("h3.DetailsSummary--daypartName--kbngc")
 		high := e.ChildText("span.DetailsSummary--highTempValue--3PjlX")
 		low := e.ChildText("span.DetailsSummary--lowTempValue--2tesQ")
-		data = append(data, WeatherData{Day: day, High: high, Low: low})
+		weatherData = append(weatherData, WeatherData{Day: day, High: high, Low: low})
 	})
 
 	err := c.Visit("https://weather.com/weather/tenday/l/fe3c78e80c47c404a4e64ec7c86ceccdb814894cedefdb528f9d8d95c3e4eb74")
@@ -35,22 +47,46 @@ func main() {
 		log.Fatal(err)
 	}
 
-	t := time.Now()
-	fileName := fmt.Sprintf("weather_%s.csv", t.Format("20060102150405"))
-	file, err := os.Create(fileName)
+	// Convert weather data to Prometheus-compatible format
+	prometheusOutput, err := transformCSVToPrometheus(weatherData)
 	if err != nil {
-		log.Fatal("Cannot create file", err)
+		log.Fatal(err)
 	}
-	defer file.Close()
 
-	writer := csv.NewWriter(file)
-	defer writer.Flush()
+	// Save weather data to a CSV file
+	t := time.Now()
+	csvFileName := fmt.Sprintf("weather_%s.csv", t.Format("20060102150405"))
+	csvFile, err := os.Create(csvFileName)
+	if err != nil {
+		log.Fatal("Cannot create CSV file:", err)
+	}
+	defer csvFile.Close()
+
+	csvWriter := csv.NewWriter(csvFile)
+	defer csvWriter.Flush()
 
 	// Write CSV header
-	writer.Write([]string{"Day", "HighTemp", "LowTemp"})
+	csvWriter.Write([]string{"Day", "HighTemp", "LowTemp"})
 
 	// Write weather data to CSV
-	for _, w := range data {
-		writer.Write([]string{w.Day, w.High, w.Low})
+	for _, w := range weatherData {
+		csvWriter.Write([]string{w.Day, w.High, w.Low})
 	}
+
+	fmt.Println("Weather data saved to", csvFileName)
+
+	// Save Prometheus-compatible output to a text file
+	prometheusFileName := fmt.Sprintf("weather_%s.txt", t.Format("20060102150405"))
+	prometheusFile, err := os.Create(prometheusFileName)
+	if err != nil {
+		log.Fatal("Cannot create Prometheus file:", err)
+	}
+	defer prometheusFile.Close()
+
+	_, err = prometheusFile.WriteString(prometheusOutput)
+	if err != nil {
+		log.Fatal("Cannot write to Prometheus file:", err)
+	}
+
+	fmt.Println("Prometheus-compatible output saved to", prometheusFileName)
 }
